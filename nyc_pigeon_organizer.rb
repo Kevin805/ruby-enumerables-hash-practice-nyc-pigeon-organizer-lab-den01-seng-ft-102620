25a36c6d7f2c2aa20e1a29a7b7364ae8ff3a662f
funny-module-5140 <kevin.michael805@gmail.com>
2020-10-19 02:59:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 def nyc_pigeon_organizer(data)
   results = data.each_with_object ({}) | (key, value) final_array|
   value.each do |inner_key, names|
-    names.each do |names|
+    names.each do |name|
+      if final_array 
   
 end
